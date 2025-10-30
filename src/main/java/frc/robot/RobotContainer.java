ff5d2ad55b08c4f087fc75e7b7b2f8b6f713c725
unknown <sesroboticsteam@gmail.com>
2025-10-29 18:24:03 -0700

    generated

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -40,7 +40,7 @@ public class RobotContainer {
         configureBindings();
     }
 
-    private void configureBindings() {
+        private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
